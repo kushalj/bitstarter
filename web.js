6426d4d91ba5795f894b3fea678438ff6166d83e
Kushal Joshi <kushal@elysian.net>
2013-07-10 20:12:59 +0000

    added Buffer

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var page = getPage('./index.html');
+var pageBuffer = new Buffer([page, "utf-8"]);
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(pageBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+var getPage = function (filename) {
+  fs.readFile('filename', function (err, data) {
+    if (err) throw err;
+    return data;
+  });
+};

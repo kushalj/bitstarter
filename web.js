58a6007faef96ddb47a5601e37ea08535e75390a
Kushal Joshi <kushal@elysian.net>
2013-07-10 20:51:19 +0000

    simplified

--- web.js
+++ web.js
@@ -1,20 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
-
-var getFileBuffer = function(filename) {
-  fileBuffer = fs.readFile(filename, function (err, data) {
-    if (err) throw err;
-    return data;
-  });
-  return fileBuffer;
-};
-
-var fileBuffer = getFileBuffer('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(fileBuffer.toString());
+   var fs = require('fs');
+   var fileBuffer = fs.readFileSync('./index.html');
+
+   response.send(fileBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
